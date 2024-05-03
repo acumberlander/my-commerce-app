ffb4f13dd18104324b5e18cbfefe7978baedbca2
Austin Cumberlander <austincumberlander@gmail.com>
2024-05-03 16:06:53 -0500

    Midsearch component is mobile responsive.

--- src/app/components/MidSearch/MidSearch.tsx
+++ src/app/components/MidSearch/MidSearch.tsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from 'react';
 import Image from "next/image";
 import searchIcon from '../../../icons/search-interface-symbol.png';
 import './MidSearch.css';
 import { Button, Input } from "@mui/material";
 
 const MidSearch = () => {
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 901
+        ? setMobileView(true)
+        : setMobileView(false);
+    };
+
+    setResponsiveness();
+
+    window.addEventListener('resize', () => setResponsiveness());
+  }, []);
+
   return (
     <div className="extra-search-div">
-      <div className="header-and-search-container">
-        <h1>Give All You Need</h1>
-        <div className="search-container-2">
-          <Image className="search-icon" src={searchIcon} alt="" />
-          <input placeholder="Search ShopyShop" />
-          <Button className="black-btn">Search</Button>
+      {
+        mobileView ? (
+          <div className="search-container-only">
+            <div className="search-container-2">
+              <Image className="search-icon" src={searchIcon} alt="" />
+              <input placeholder="Search ShopyShop" />
+              <Button className="black-btn">Search</Button>
+            </div>
+          </div>
+        ): (
+        <div className="header-and-search-container">
+          <h1>Give All You Need</h1>
+          <div className="search-container-2">
+            <Image className="search-icon" src={searchIcon} alt="" />
+            <input placeholder="Search ShopyShop" />
+            <Button className="black-btn">Search</Button>
+          </div>
         </div>
-      </div>
+        )
+      }
+      
     </div>
   )
 }
