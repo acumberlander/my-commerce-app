44ca1a296a55601cd1e2e2ca49984a3a69967eea
Austin Cumberlander <austincumberlander@gmail.com>
2024-05-07 14:01:23 -0500

    test

--- src/app/utils/firebase.ts
+++ src/app/utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBo-lzqZ0ZEuBND0mUOeBvTHOvrbHG5y20",
-  authDomain: "shopyshop-2f681.firebaseapp.com",
-  projectId: "shopyshop-2f681",
-  storageBucket: "shopyshop-2f681.appspot.com",
-  messagingSenderId: "706025276618",
-  appId: "1:706025276618:web:c75a5fbb78702b5305e033",
-  measurementId: "G-M6CLN8XZX2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
